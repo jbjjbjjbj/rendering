41ea2d8f94043d49fc26aaaa8030a73dcfdc2da9
Julian T <julian@jtle.dk>
2021-01-24 18:13:44 +0100

    Create rust project

--- src/common.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef COMMON_H
-#define COMMON_H
-
-#define ZERO_APPROX 1e-6
-
-#endif
--- src/scene.cpp
+++
@@ -1,5 +0,0 @@
-#include "scene.hpp"
-
-void Scene::addShape(const Shape *obj) {
-    objs.push_back(obj);
-}
